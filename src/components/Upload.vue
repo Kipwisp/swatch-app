bc5df3b251ed3e512396961208dee4faa2daf796
Kipwisp <61100140+Kipwisp@users.noreply.github.com>
2022-03-14 01:28:24 -0500

    set API url to env variable

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -52,10 +52,7 @@ export default defineComponent({
   },
   methods: {
     async uploadFile(file: File) {
-      const apiURL =
-        process.env.NODE_ENV === "development"
-          ? "http://127.0.0.1:5000/analyze"
-          : "";
+      const apiURL = process.env.VUE_APP_API_URL;
 
       try {
         this.uploading = true;
